3b975334a6e78e75423334afc373231aae40f944
Sebastian P <sebastian2112mejia@hotmail.com>
2026-02-23 19:09:56 -0600

    Actualizando

--- src/server.js
+++ src/server.js
@@ -14,6 +14,9 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 // 2. IMPORTANTE: Configuración BLINDADA de la carpeta pública 🛡️
 // Le decimos: "Desde donde estoy (__dirname), sube un nivel (..) y busca 'public'"
 app.use(express.static(path.join(__dirname, '../public')));
