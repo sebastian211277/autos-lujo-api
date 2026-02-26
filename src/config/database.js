1f5e8c8c0b7da09704ecc8fed0f207b4df9b8294
Sebastian P <sebastian2112mejia@hotmail.com>
2026-02-25 19:23:23 -0600

    New Update

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const URI = "mongodb+srv://shyrio:Password123@cluster0.xxxxx.mongodb.net/luxury-cars?retryWrites=true&w=majority";
 
 const connectDB = async () => {
